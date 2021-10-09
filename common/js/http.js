97eb7a50b9abd94936a59c617063f6e8208703e7
MonkeyQiang <15172750633@163.com>
2021-10-09 11:46:35 +0800

    hzq -- 请求超时显示动画时间修改

--- common/js/http.js
+++ common/js/http.js
@@ -7,7 +7,7 @@ const install = (Vue, vm) => {
 		// baseUrl:'http://8.129.187.233:25676',
 		showLoading: true, // 是否显示请求中的loading
 		loadingText: '请求中...', // 请求loading中的文字提示
-		loadingTime: 1500, // 在此时间内，请求还没回来的话，就显示加载中动画，单位ms
+		loadingTime: 3000, // 在此时间内，请求还没回来的话，就显示加载中动画，单位ms
 		originalData: true, // 是否在拦截器中返回服务端的原始数据
 		loadingMask: true, // 展示loading的时候，是否给一个透明的蒙层，防止触摸穿透
 		// 设置自定义头部content-type
